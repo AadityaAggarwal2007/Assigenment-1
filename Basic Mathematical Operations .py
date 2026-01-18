7b3632634b1f45b6d339860908fb27a664223b91
Aaditya Aggarwal <aadityaaggarwal3526@gmail.com>
2026-01-19 00:26:09 +0530

    Add files via upload

---
+++ Basic Mathematical Operations .py
@@ -0,0 +1,10 @@
+#Task 1: Perform Basic Mathematical Operations
+
+A = float(input("First number: "))
+B = float(input("Second number: "))
+print("Addition:",A+B)
+print("Subtraction:",A-B)
+print("Multiplication:",A*B)
+print("Division:",A/B)
+print("Exponent:",A**B)
+
---
+++ Create a Personalized Greeting.py
@@ -0,0 +1,4 @@
+#Create a Personalized Greeting
+A = input("First name:")
+B = input("Second name:")
+print(f"Hello, {A} {B} ! Welcome to Python Program.")
